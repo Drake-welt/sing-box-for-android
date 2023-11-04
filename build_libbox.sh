ce412b91d07e0b2c89523de18b09d245d837d7ed
Drake <147331678+Welt-Drake@users.noreply.github.com>
2023-11-05 04:40:51 +0800

    Update build_libbox.sh

--- build_libbox.sh
+++ build_libbox.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 ##### 除非你知道自己在干什么，否则什么都不要动 #####
 # 想使用指定分支请修改链接，注意不要去掉后面的`sing-box`
-git clone https://github.com/rnetx/sing-box sing-box
+git clone https://github.com/reF1nd/sing-box sing-box
 ###### !! 不要动 !! ######
 cd sing-box
 ##########################
 # 这里可以切换分支
-git checkout dev
+git checkout dev-routestrategy
 ##########################
 # 编译 with_clash_ui 需要，其他分支可以注释掉 #########
-make init_yacd
+#make init_metacubexd
 ####################
 # 这里可以修改 tags
-tags="with_dhcp,with_lwip,with_gvisor,with_v2ray_api,with_quic,with_wireguard,with_utls,with_reality_server,with_clash_api,with_grpc,with_ech,with_proxyprovider,with_ruleprovider,with_clash_dashboard"
-####################
+tags="with_dhcp,with_gvisor,with_quic,with_utls,with_clash_api,with_proxyprovider,with_ruleprovider,with_clash_ui"
 ###### !! 不要动 !! ######
 make lib_install
 version=$(CGO_ENABLED=0 go run ./cmd/internal/read_tag)
-CGO_ENABLED=1 gomobile bind -v -androidapi 21 -javapkg=io.nekohasekai -libname=box -tags ${tags} -ldflags "-X github.com/sagernet/sing-box/constant.Version=${version} -buildid=" ./experimental/libbox
+CGO_ENABLED=0 gomobile bind -v -androidapi 21 -javapkg=io.nekohasekai -libname=box -tags ${tags} -ldflags "-X github.com/sagernet/sing-box/constant.Version=${version} -buildid=" ./experimental/libbox
 ##########################
